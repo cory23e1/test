8a4e74b8af018f664cc273c2160201f346e0e08a
cory23e1 <cory23@yandex.ru>
2024-08-06 23:38:28 +0400

    third commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  // $("#article1").on("click", function(){
+  //
+  //   $.post("../data/lessons/lesson1/index.html", function(data){
+  //
+  //     $("body").html(data);
+  //     // $(".modal-body").html(data).fadeIn();
+  //
+  //   });
+  //
+  // });
+
+});
